527b900da505a703f526431ee89948a2edb1bd6a
Sergio <Sergulai@yandex.ru>
2026-09-17 00:08:18 +0300

    Убрал неиспользованные imports.

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item;
 
 import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.request.ItemRequest;
 
 @UtilityClass
 public class ItemMapper {
